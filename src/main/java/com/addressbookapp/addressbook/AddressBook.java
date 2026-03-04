7a7ac0c66f725802da37b35df176267c91ab22b3
rudresh-sharma <rudresh.sharma.8602@gmail.com>
2026-03-04 10:17:50 +0530

    [Rudresh] : Added Functionalities

--- src/main/java/com/addressbookapp/addressbook/AddressBook.java
+++ src/main/java/com/addressbookapp/addressbook/AddressBook.java
@@ -7,7 +7,14 @@ import java.util.Scanner;
 public class AddressBook {
 
     private List<Contact> contactList = new ArrayList<>();
-
+    
+    
+    public List<Contact> getContactList() {
+        return contactList;
+    }
+    
+    
+    
     public void addContact(Contact contact) {
         contactList.add(contact);
         System.out.println("Contact added successfully!");
