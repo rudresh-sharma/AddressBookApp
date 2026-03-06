4f9585892e631e5f74ac79332050b704ee97858c
rudresh-sharma <rudresh.sharma.8602@gmail.com>
2026-03-06 12:06:55 +0530

    [Rudresh] : refactor(model): migrate Contact to Lombok @Data with generated constructors

--- src/main/java/com/addressbookapp/model/Contact.java
+++ src/main/java/com/addressbookapp/model/Contact.java
@@ -1,5 +1,12 @@
 package com.addressbookapp.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Contact {
 
     private String firstName;
@@ -11,95 +18,18 @@ public class Contact {
     private String phoneNumber;
     private String email;
 
-    public Contact() {
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+
+        Contact contact = (Contact) obj;
+        return firstName.equalsIgnoreCase(contact.firstName)
+                && lastName.equalsIgnoreCase(contact.lastName);
     }
 
-    public Contact(String firstName, String lastName, String address,
-                   String city, String state, String zip,
-                   String phoneNumber, String email) {
-
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.address = address;
-        this.city = city;
-        this.state = state;
-        this.zip = zip;
-        this.phoneNumber = phoneNumber;
-        this.email = email;
+    @Override
+    public int hashCode() {
+        return (firstName.toLowerCase() + lastName.toLowerCase()).hashCode();
     }
-    public String getFirstName() {
-		return firstName;
-	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	public String getLastName() {
-		return lastName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-	public String getAddress() {
-		return address;
-	}
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
-	public String getCity() {
-		return city;
-	}
-	public void setCity(String city) {
-		this.city = city;
-	}
-	public String getState() {
-		return state;
-	}
-
-	public void setState(String state) {
-		this.state = state;
-	}
-	public String getZip() {
-		return zip;
-	}
-	public void setZip(String zip) {
-		this.zip = zip;
-	}
-	public String getPhoneNumber() {
-		return phoneNumber;
-	}
-	public void setPhoneNumber(String phoneNumber) {
-		this.phoneNumber = phoneNumber;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	@Override
-    public String toString() {
-        return firstName + " " + lastName + ", " +
-                address + ", " + city + ", " + state +
-                " - " + zip + ", Phone: " + phoneNumber +
-                ", Email: " + email;
-    }
-	
-	@Override
-	public boolean equals(Object obj) {
-
-	    if (this == obj) return true;
-
-	    if (obj == null || getClass() != obj.getClass()) return false;
-
-	    Contact contact = (Contact) obj;
-
-	    return this.firstName.equalsIgnoreCase(contact.firstName) &&
-	           this.lastName.equalsIgnoreCase(contact.lastName);
-	}
-
-	@Override
-	public int hashCode() {
-	    return (firstName.toLowerCase() + lastName.toLowerCase()).hashCode();
-	}
 }
